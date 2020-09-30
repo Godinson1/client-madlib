ceb83670939c59802f1fa82647c80f68ffae9b26
unknown <josephgodwin274@yahoo.com>
2020-09-30 01:49:53 +0100

    refactor

--- src/store.js
+++ src/store.js
@@ -8,9 +8,7 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    process.env.NODE_ENV !== "production" &&
-      typeof window !== "undefined" &&
-      applyMiddleware(...middleware),
+    applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__
       ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
           window.__REDUX_DEVTOOLS_EXTENSION__()
